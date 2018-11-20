2707190fd26d9bd1c540c918652b4f02491a2ffc
Karen Gomes <karen.nayara@audora.com.br>
2018-11-20 11:28:57 -0300

    cat clicker 2 done

--- cat-clicker/app.js
+++ cat-clicker/app.js
@@ -1,7 +1,14 @@
-let element = document.getElementById('cat-image');
+let elements = document.querySelectorAll(".cat-image");
+let arrayCatNames = ["Ryan", "Chewie"];
+let elemCatName = document.querySelectorAll(".catName");
+for (var i = 0; i < arrayCatNames.length; i++) {
+  elemCatName[i].textContent = arrayCatNames[i];
+}
 let count = 0;
-element.addEventListener('click', () => {
+for (let elem of elements) {
+  elem.addEventListener("click", () => {
     count++;
-    countElem = document.getElementById('count');
+    countElem = document.getElementById("count");
     countElem.textContent = Number(count);
-});
+  });
+}
